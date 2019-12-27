4cec21758daf9d1634694f35d615d544f40bb5cb
vvxforce <vvxforce@gmail.com>
2019-12-27 14:22:07 +0200

    Updates

---
+++ precache-manifest.8e6d79d59758122ee9a9b4794ce7eaea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485e67d8c5ae85c418366f596487dbb5",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "9c6ed4821900a47168a3",
+    "url": "/react_tabs-with-router/static/css/main.3bc91780.chunk.css"
+  },
+  {
+    "revision": "2e0d167d9f7fac2c8b72",
+    "url": "/react_tabs-with-router/static/js/2.9eb9364c.chunk.js"
+  },
+  {
+    "revision": "9c6ed4821900a47168a3",
+    "url": "/react_tabs-with-router/static/js/main.1ee593f2.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
